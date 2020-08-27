61c352383379aa64db6e8bb43c6514cc1961f108
akhilsathuluri <akhilsathuluri@gmail.com>
2020-08-27 21:26:46 +0530

    hole_detect_working

--- remote/remote/demo.py
+++ remote/remote/demo.py
@@ -20,7 +20,7 @@ if model_name == 'resnet':
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 4)
 
-    model.load_state_dict(torch.load("trained/new/tank_models_hole.pth"))
+    model.load_state_dict(torch.load("trained/new/tank_model_ident_res18_fe_retrain_light_hole.pth"))
     model.eval()
     print('Resnet model initialised...')
 
@@ -28,7 +28,10 @@ if model_name == 'squeeze':
     model = models.squeezenet1_0(pretrained=True)
     model.classifier[1] = nn.Conv2d(512, 4, kernel_size=(1,1), stride=(1,1))
 
-    model.load_state_dict(torch.load("trained/new/tank_model_ident_squeeze_fe.pth"))
+    # if trt_model
+    model.load_state_dict(torch.load("trained/new/squeeze_trt.pth"))
+    # if non_trt_model
+    # model.load_state_dict(torch.load("trained/new/tank_model_ident_squeeze_fe.pth"))
     model.eval()
     print('Squeezenet model initialised...')
 
@@ -41,6 +44,9 @@ if model_name == 'mobile':
     model.eval()
     print('Mobilenet_v2 model initialised...')
 
+if model_name == 'none':
+    pass
+
 class_index = json.load(open('classes.json'))
 print('classes loaded...')
 
@@ -49,6 +55,16 @@ bottomLeftCornerOfText = (200,450)
 fontScale              = 2
 fontColor              = (255,0,0)
 lineType               = 10
+
+thickness = 10
+ok_color = (0,255,0)
+nok_color = (0,0,255)
+wait_color = (255,0,0)
+start_point = (5,5)
+end_point = (220,220)
+x_crop = 360
+y_crop = 200
+window = 300
 print('settings loaded...')
 
 template = cv2.imread('background.png')
@@ -56,16 +72,45 @@ _, w, h = template.shape[::-1]
 method = eval('cv2.TM_CCOEFF')
 print('background template and method loaded...')
 
+
 # Component precense
 def check_presence(frame):
-    res = cv2.matchTemplate(frame,template,method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    print(max_val)
-    if max_val > 11454307000:
-        comp_present = 0
+    # res = cv2.matchTemplate(frame,template,method)
+    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    # print(max_val)
+    # if max_val > 11454307000:
+    #     comp_present = 0
+    # else:
+    #     comp_present = 1
+    # return comp_present
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    frame_crop = frame[y_crop:y_crop+window,x_crop:x_crop+window]
+    circles = cv2.HoughCircles(frame_crop, cv2.HOUGH_GRADIENT, 1.2, 100)
+    # cv2.rectangle(frame, (y_crop, x_crop), (y_crop+window,x_crop+window), (255, 255, 255), -1)
+    # if circles is not None:
+    #     circles = np.round(circles[0, :]).astype("int")
+    #     # if len(circles!=1)
+    #     for (x, y, r) in circles:
+    #         cv2.circle(frame, (x+x_crop, y+y_crop), r, (255, 255, 255), 30)
+    #         cv2.rectangle(frame, (x+260 - 5, y+200 - 5), (x+260 + 5, y+200 + 5), (255, 255, 255), -1)
+    if circles is not None:
+        return 1, circles
     else:
-        comp_present = 1
-    return comp_present
+        return 0, 0
+    # return frame
+
+def draw_circle(frame, circles):
+    circles = np.round(circles[0, :]).astype("int")
+    for (x, y, r) in circles:
+        cv2.circle(frame, (x+x_crop, y+y_crop), r, (0, 255, 0), 30)
+        # cv2.rectangle(frame, (x+260 - 5, y+200 - 5), (x+260 + 5, y+200 + 5), (255, 255, 255), -1)
+        
+    return frame
+    
+def draw_roi(frame):
+    cv2.rectangle(frame, (x_crop, y_crop), (x_crop + window, y_crop + window), (255, 255, 255), 3)    
+    return frame
 
 def transform_image(img):
     my_transforms = transforms.Compose([transforms.Resize(255),
@@ -97,40 +142,32 @@ frames = 1
 a = datetime.now()
 while True:
     ret, frame = cam.read()
-    tensor = transform_image(frame)
-    # print("obtaining prediction...")
-    pred, prob = get_prediction(tensor)
-    status = check_presence(frame)
-    if status==1:
-        if prob > 0.45:
-            # cv2.putText(frame,pred+':{}'.format(prob)+'%',
-            #     bottomLeftCornerOfText,
-            #     font,
-            #     fontScale,
-            #     fontColor,
-            #     lineType)
+    present, circles = check_presence(frame)
+    if present == 1:
+        if model_name is not 'none':
+            tensor = transform_image(frame)
+            print("obtaining prediction...")
+            pred, prob = get_prediction(tensor)
             cv2.putText(frame,pred,
-                bottomLeftCornerOfText,
-                font,
-                fontScale,
-                fontColor,
-                lineType)
-        else:
-            cv2.putText(frame,"Please wait",
-                bottomLeftCornerOfText,
-                font,
-                fontScale,
-                fontColor,
-                lineType)
-    if status==0:
-        cv2.putText(frame,"Waiting for tank",
-                bottomLeftCornerOfText,
-                font,
-                fontScale,
-                fontColor,
-                lineType)
+            bottomLeftCornerOfText,
+            font,
+            fontScale,
+            fontColor,
+            lineType)
+        frame = draw_circle(frame, circles)
 
+    if present==0:
+        cv2.putText(frame,"Waiting for tank",
+            bottomLeftCornerOfText,
+            font,
+            fontScale,
+            fontColor,
+            lineType)
+    
+    frame = draw_roi(frame)
+    # frame_crop = frame[y_crop:y_crop+window,x_crop+100:x_crop+window+100]
     cv2.imshow('Display', frame)
+    # cv2.imshow('Full', frame)
     frames = frames + 1
 
     k = cv2.waitKey(1)
